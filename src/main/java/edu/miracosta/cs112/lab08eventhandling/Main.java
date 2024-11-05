9f58fe2c4607507e60261e8898c6ad69dd3e9dd3
Code-Sing <codysingairforce@gmail.com>
2024-11-04 23:55:02 -0800

    Update Main.java

--- src/main/java/edu/miracosta/cs112/lab08eventhandling/Main.java
+++ src/main/java/edu/miracosta/cs112/lab08eventhandling/Main.java
@@ -1,8 +1,23 @@
 package edu.miracosta.cs112.lab08eventhandling;
 
 //IMPORTED PACKAGES
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.geometry.Pos;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;              //class for GUI window
+import javafx.scene.Scene;              //class for specific view in GUI window
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
+import javafx.scene.control.Label;      //class for label component
+import javafx.scene.control.Button;     //class for button component
+import javafx.event.EventHandler;       //interface for handling events
+import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Main extends Application {
     //CONSTANTS
@@ -15,8 +30,20 @@ public class Main extends Application {
 
     //CLASS-LEVEL VARIABLES
 
+    private int counter = 0;
+    private LoteriaCard[] randomCards;
+
+    //GUI COMPONENTS
+
+    private Button drawCardButton;
+    private Label titleLabel, messageLabel;
+    private ProgressBar gameProgressBar;
+    private ImageView cardImageView;
+
 
     //GUI METHODS
+
+    //Driver
     public static void main(String[] args) {
         launch(args);
     }
@@ -24,5 +51,67 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        //Setup
+        LoteriaCard startupCard = new LoteriaCard();
+        cardImageView = new ImageView(startupCard.getImage());
+        cardImageView.setFitHeight(238);
+        cardImageView.setPreserveRatio(true);
+
+        //Shuffle Card
+        randomCards = new LoteriaCard[LOTERIA_CARDS.length];
+        randomCards = LOTERIA_CARDS.clone();
+        List<LoteriaCard> list = Arrays.asList(randomCards);
+        Collections.shuffle(list);
+        list.toArray(randomCards);
+
+
+
+        drawCardButton = new Button("Draw Random Card");
+        drawCardButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent){
+                    if(counter < randomCards.length){
+                        cardImageView.setImage(randomCards[counter].getImage());
+                        messageLabel.setText(randomCards[counter].getCardName());
+                        gameProgressBar.setProgress((double) (counter +1) / LOTERIA_CARDS.length);
+
+                    } else if (counter == randomCards.length) {
+                        drawCardButton.setDisable(true);
+                        cardImageView.setImage(startupCard.getImage());
+                        gameProgressBar.setStyle("-fx-accent: red;");
+                        messageLabel.setText("GAME OVER. No more cards! Exit and run program again to reset ^_^");
+                    }
+                    counter++;
+
+
+                }
+            }
+
+
+        ); //Event handling
+
+        titleLabel = new Label("Welcome to EChALE STEM Loteria!");
+        titleLabel.setFont( new Font(20));
+
+        messageLabel = new Label("Click the button to draw a random card." +
+                " The progress bar shows how many more cards you can draw.");
+        messageLabel.setWrapText(true);
+        messageLabel.setTextAlignment(TextAlignment.CENTER);
+
+        gameProgressBar = new ProgressBar();
+
+        //Add
+
+        VBox layout = new VBox(25);
+        layout.setAlignment(Pos.TOP_CENTER);
+        layout.getChildren().addAll(titleLabel,cardImageView,messageLabel,drawCardButton,gameProgressBar);
+
+        //Setup Scene And Show
+        Scene primaryScene = new Scene(layout, 350,500);
+        primaryStage.setScene(primaryScene);
+        primaryStage.setTitle("EChALE STEM LOTERIA");
+        primaryStage.show();
+
     }
+
 }
